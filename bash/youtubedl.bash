07bf31c9a12401a209c6fdaa8267f17475a6862b
Joschka Koester <jkoester@tzi.de>
2016-08-14 14:10:59 +0200

    [Youtube DL]
    Script for downloading a youtube video's audio as mp3.

---
+++ bash/youtubedl.bash
@@ -0,0 +1,24 @@
+#!/bin/bash 
+# Script for grabbing the audio of youtube videos as mp3. 
+address=$1
+bitrate=$2
+default_bitrate=256
+title=$(youtube-dl --get-title $address)
+filename=$(youtube-dl --get-filename $address)
+youtube-dl $address
+mkdir .tmp
+mv *.mkv .tmp
+cd .tmp
+
+shopt -s nullglob
+
+array=(*)
+
+for i in "${array[@]}"
+do
+ffmpeg -i "$i" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "$i.mp3"
+done
+
+mv *.mp3 ../
+cd ..
+rm -r .tmp
