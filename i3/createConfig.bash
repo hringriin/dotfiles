ae8dd76ea3617d9e9c81347a533a3c655f15c06b
Joschka Koester <jkoester@tzi.de>
2016-11-07 20:48:16 +0100

    createi3Config script
    will add a script for creating the host specifig i3 config, based on the
    given files in the git directory i3/i3/

---
+++ i3/createConfig.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+# createConfig - creates the host specifig i3 config
+# this will only work for my personal computers
+
+REPOPATH="/home/hringriin/Repositories/github.com/hringriin/dotfiles/repo/i3/i3"
+I3GLOBAL="config"
+I3HOST=`echo ${HOSTNAME}`
+HOSTCONF="/home/hringriin/.config/i3/conf-test"
+
+echo "Repository path                       : ${REPOPATH}"
+echo "I3 global config                      : ${I3GLOBAL}"
+echo "I3 host specifig config               : ${I3HOST}"
+echo "I3 final i3 config for specifig host  : ${HOSTCONF}"
+
+echo -e "\n"
+read -p "Write to ${HOSTCONF} ? [Y/n] " writeToConf
+
+if [[ ${writeToConf} = "y" || ${writeToConf} = "Y" || ${writeToConf} = "" ]] ; then
+    echo -e "\nWriting to config ..."
+    rm -f ${HOSTCONF}
+    cat ${REPOPATH}/${I3GLOBAL} >> ${HOSTCONF}
+    echo -e "\n\n" >> ${HOSTCONF}
+    cat ${REPOPATH}/${I3HOST} >> ${HOSTCONF}
+    echo -e " ... finished!"
+elif [[ ${writeToConf} = "n" || ${writeToConf} = "N" ]] ; then
+    echo -e "\nWill not touch anything!"
+fi
