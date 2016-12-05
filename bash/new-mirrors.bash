42e8f52852195c04413824c1851eca4b2a1505e8
Joschka Koester <jkoester@tzi.de>
2016-12-05 16:57:58 +0100

    mirrorlist update script

---
+++ bash/new-mirrors.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# new-mirros - recalculates the arch linux mirrorlist
+
+PMD='/etc/pacman.d'
+PML='mirrorlist'
+PMN='mirrorlist.pacnew'
+PMT='mirrorlist.tmp'
+
+if [[ -e ${PMD}/${PMN} ]] ; then
+    cp -iv ${PMD}/${PMN} ${PMD}/${PMT}
+elif [[ -e ${PMD}/${PML} ]] ; then
+    cp -iv ${PMD}/${PML} ${PMD}/${PMT}
+fi
+
+sed -i 's/^#Server/Server/' ${PMD}/${PMT}
+
+rankmirrors -n 10 ${PMD}/${PMT} > ${PMD}/${PML}
+
+pacman -Syyu
+
+rm -rfv ${PMD}/${PMT}
+rm -rfv ${PMD}/${PMN}
