001eb5c2e7b6d375857690a202f392f09a5b9cf3
Joschka Koester <jkoester@tzi.de>
2016-12-20 04:58:55 +0100

    weechat config linker script works

--- weechat/link-files.bash
+++ weechat/link-files.bash
@@ -1,23 +1,31 @@
 #!/bin/bash
 # link-files
 
-wcpath='~/.weechat'
-srcpath='~/Repositories/github.com/hringriin/dotfiles/repo/weechat'
+# Config
+if [[ ${HOSTNAME} == "botis" ]] ; then          # check if it is my only Mac Os Machine by hostname
+    wcpath='/Users/hringriin/.weechat'
+    srcpath='/Users/hringriin/Repositories/github.com/hringriin/dotfiles/repo/weechat'
+else
+    wcpath='/home/hringriin/.weechat'
+    srcpath='/home/hringriin/Repositories/github.com/hringriin/dotfiles/repo/weechat'
+fi
 
-rm -r ${wcpath}/alias.conf
-rm -r ${wcpath}/aspell.conf
-rm -r ${wcpath}/charset.conf
-rm -r ${wcpath}/exec.conf
-rm -r ${wcpath}/irc.conf
-rm -r ${wcpath}/logger.conf
-rm -r ${wcpath}/plugins.conf
-rm -r ${wcpath}/relay.conf
-rm -r ${wcpath}/script.conf
-rm -r ${wcpath}/sec.conf
-rm -r ${wcpath}/trigger.conf
-rm -r ${wcpath}/weechat.conf
-rm -r ${wcpath}/xfer.conf
+# remove all config files
+rm -f ${wcpath}/alias.conf
+rm -f ${wcpath}/aspell.conf
+rm -f ${wcpath}/charset.conf
+rm -f ${wcpath}/exec.conf
+rm -f ${wcpath}/irc.conf
+rm -f ${wcpath}/logger.conf
+rm -f ${wcpath}/plugins.conf
+rm -f ${wcpath}/relay.conf
+rm -f ${wcpath}/script.conf
+rm -f ${wcpath}/sec.conf
+rm -f ${wcpath}/trigger.conf
+rm -f ${wcpath}/weechat.conf
+rm -f ${wcpath}/xfer.conf
 
+# link the config files from the repository
 ln -s ${srcpath}/alias.conf ${wcpath}/alias.conf
 ln -s ${srcpath}/aspell.conf ${wcpath}/aspell.conf
 ln -s ${srcpath}/charset.conf ${wcpath}/charset.conf
