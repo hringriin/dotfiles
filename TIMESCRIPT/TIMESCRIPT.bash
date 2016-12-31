d34ba92ab348ffe3c6d1bf5f1b84c8ccbcb3946c
Joschka Koester <jkoester@tzi.de>
2017-01-01 00:40:03 +0100

    timescript for `sm`

---
+++ TIMESCRIPT/TIMESCRIPT.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# String for tutorium end format "<HOUR><HOUR>:<MINUTE><MINUTE>:<SECOND><SECOND>"
+TUTEND="17:45:00"
+
+
+
+# DO NOT TOUCH
+NEWEND=`date -d "${TUTEND}" "+%s"`
+NEWNOW=`date "+%s"`
+NEWREMAINING=$[${NEWEND} - ${NEWNOW} - 3600]                # output of `date -d @10 "+%T"` == 1 hour 10 seconds --> -= 1 Hour
+
+if [[ ${NEWREMAINING} -lt -3600 ]] ; then
+    echo -e "`date "+%T"` Uhr\n"
+    echo -e "Zeit aufgebraucht!"
+else
+    echo -e "`date "+%T"` Uhr\n"
+    echo -e "Restzeit: `date -d @${NEWREMAINING} "+%T"`"
+fi
