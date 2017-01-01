56375279c9e7356b30f6c5572aa2fd0e713c1455
Joschka Koester <jkoester@tzi.de>
2017-01-01 13:48:21 +0100

    weechat link configs updated

--- weechat/link-files.bash
+++ weechat/link-files.bash
@@ -1,43 +1,48 @@
 #!/bin/bash
 # link-files
 
+wcpath=""
+srcpath=""
+
 # Config
 if [[ ${HOSTNAME} == "botis" ]] ; then          # check if it is my only Mac Os Machine by hostname
-    wcpath='/Users/hringriin/.weechat'
-    srcpath='/Users/hringriin/Repositories/github.com/hringriin/dotfiles/repo/weechat'
+    wcpath="/Users/${USER}/.weechat"
+    srcpath="/Users/${USER}/Repositories/github.com/hringriin/dotfiles/repo/weechat"
 else
-    wcpath='/home/hringriin/.weechat'
-    srcpath='/home/hringriin/Repositories/github.com/hringriin/dotfiles/repo/weechat'
+    wcpath="/home/${USER}/.weechat"
+    srcpath="/home/${USER}/Repositories/github.com/hringriin/dotfiles/repo/weechat"
 fi
 
-# remove all config files
-rm -f ${wcpath}/alias.conf
-rm -f ${wcpath}/aspell.conf
-rm -f ${wcpath}/charset.conf
-rm -f ${wcpath}/exec.conf
-rm -f ${wcpath}/irc.conf
-rm -f ${wcpath}/logger.conf
-rm -f ${wcpath}/plugins.conf
-rm -f ${wcpath}/relay.conf
-rm -f ${wcpath}/script.conf
-rm -f ${wcpath}/sec.conf
-rm -f ${wcpath}/trigger.conf
-rm -f ${wcpath}/weechat.conf
-rm -f ${wcpath}/xfer.conf
-
 # link the config files from the repository
-ln -s ${srcpath}/alias.conf ${wcpath}/alias.conf
-ln -s ${srcpath}/aspell.conf ${wcpath}/aspell.conf
-ln -s ${srcpath}/charset.conf ${wcpath}/charset.conf
-ln -s ${srcpath}/exec.conf ${wcpath}/exec.conf
-ln -s ${srcpath}/irc.conf ${wcpath}/irc.conf
-ln -s ${srcpath}/logger.conf ${wcpath}/logger.conf
-ln -s ${srcpath}/plugins.conf ${wcpath}/plugins.conf
-ln -s ${srcpath}/relay.conf ${wcpath}/relay.conf
-ln -s ${srcpath}/script.conf ${wcpath}/script.conf
-ln -s ${srcpath}/trigger.conf ${wcpath}/trigger.conf
-ln -s ${srcpath}/weechat.conf ${wcpath}/weechat.conf
-ln -s ${srcpath}/xfer.conf ${wcpath}/xfer.conf
+
+if [[ ! -d ${wcpath} ]] ; then
+    mkdir ${wcpath}
+fi
+
+ln -fsv ${srcpath}/alias.conf ${wcpath}/alias.conf
+ln -fsv ${srcpath}/aspell.conf ${wcpath}/aspell.conf
+ln -fsv ${srcpath}/charset.conf ${wcpath}/charset.conf
+ln -fsv ${srcpath}/exec.conf ${wcpath}/exec.conf
+ln -fsv ${srcpath}/irc.conf ${wcpath}/irc.conf
+ln -fsv ${srcpath}/logger.conf ${wcpath}/logger.conf
+ln -fsv ${srcpath}/plugins.conf ${wcpath}/plugins.conf
+ln -fsv ${srcpath}/relay.conf ${wcpath}/relay.conf
+ln -fsv ${srcpath}/script.conf ${wcpath}/script.conf
+ln -fsv ${srcpath}/trigger.conf ${wcpath}/trigger.conf
+ln -fsv ${srcpath}/weechat.conf ${wcpath}/weechat.conf
+ln -fsv ${srcpath}/xfer.conf ${wcpath}/xfer.conf
 
 # the sec.conf has to be copied and decrypted
 cp ${srcpath}/sec.conf ${wcpath}/sec.conf
+
+echo -e "Please decrypt the sec.conf file with vim"
+read -p "Do it now? [Y/n]: " decrypt
+
+if [[ ${decrypt} == "y" || ${decrypt} == "Y" || ${decrypt} == "" ]] ; then
+    vim ${wcpath}/sec.conf
+elif
+    [[ ${decrypt} == "n" || ${decrypt} == "N" ]] ; then
+    echo -e "Will not touch sec.conf.\nPlease decrypt it by yourself before starting weechat, otherwise it will be overwritten!"
+else
+    echo -e "Unrecognized entry."
+fi
