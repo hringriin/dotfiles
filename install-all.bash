546ef3546360d1acb9a382e916a15bedebc66b10
Joschka Koester <jkoester@tzi.de>
2017-01-01 13:55:13 +0100

    install-all script finished

--- install-all.bash
+++ install-all.bash
@@ -11,16 +11,19 @@
 # 0: everything fine
 # 1: user chose not to continue at Arch Linux Question
 # 2: incorrect values entered
+# 3: user will not be added to sudoers file
 
 # temporary files
 tmpDir="/tmp"                                                   # temp dir
 tmp1="neededProgrammes$$"                                       # temp file for evaluation of needed programmes by pacman
 tmp2="neededProgrammes2$$"                                      # temp file for evaluation of needed programmes by packer
+tmp3="sudoers"                                                  # temp file for evaluation of member of sudoers
 missingPacmanPrg=0                                              # boolean; 0: all installed; 1: one or more programmes missing
 missingPackerPrg=0                                              # boolean; 0: all installed; 1: one or more programmes missing
 missingPacmanList="missingPacmanList$$"                         # list of missing programmes (pacman)
 missingPackerList="missingPackerList$$"                         # list of missing programmes (packer)
-repoPath="~/Repositories/github.com/hringriin/dotfiles/repo"    # path of the repository
+packerList=()
+repoPath="/home/${USER}/Repositories/github.com/hringriin/dotfiles/repo"    # path of the repository
 ulbin="/usr/local/bin"
 
 # list of programmes maintained by packer needed for the whole action
@@ -151,9 +154,9 @@ function archLinuxCheck()
 
 # checks whether programmes are missing and writes those
 # names to a file
-function checkNeededProgrammes()
+function checkNeededPacman()
 {
-    cleanup
+    echo -e "\n\nChecking for not installed programmes (pacman) ...\n\n"
 
     # checks every programm in the array whether it is installed
     for var in "${neededProgrammes[@]}"
@@ -170,23 +173,6 @@ function checkNeededProgrammes()
     # nice output with column. green=installed;red=notinstalled
     echo -e "\n\n\nList of Programmes provided by \e[93mpacman\e[0m\n"
     column -t -s "..." ${tmpDir}/${tmp1}
-    sleep 1     # just to slow things a bit down
-
-    # checks every programm in the array whether it is installed
-    for var in "${neededPackerPrgorammes[@]}"
-    do
-        if pacman -Qi ${var} &> /dev/null ; then
-            echo -e "${var} ... \e[92minstalled\e[0m" >> ${tmpDir}/${tmp2}
-        else
-            echo -e "${var} ... \e[91mnot installed\e[0m" >> ${tmpDir}/${tmp2}
-            echo ${var} >> ${tmpDir}/${missingPackerList}
-            missingPackerPrg=1
-        fi
-    done
-
-    # nice output with column. green=installed;red=notinstalled
-    echo -e "\n\n\nList of Programmes provided by \e[93mpacker\e[0m\n"
-    column -t -s "..." ${tmpDir}/${tmp2}
     sleep 1     # just to slow things a bit down
 
     # if there are programmes missing (pacman)
@@ -210,6 +196,28 @@ function checkNeededProgrammes()
     fi
 
     unset instMissPrg
+}
+
+function checkNeededPacker()
+{
+    echo -e "\n\nChecking for not installed programmes (packer) ...\n\n"
+
+    # checks every programm in the array whether it is installed
+    for var in "${neededPackerPrgorammes[@]}"
+    do
+        if pacman -Qi ${var} &> /dev/null ; then
+            echo -e "${var} ... \e[92minstalled\e[0m" >> ${tmpDir}/${tmp2}
+        else
+            echo -e "${var} ... \e[91mnot installed\e[0m" >> ${tmpDir}/${tmp2}
+            echo ${var} >> ${tmpDir}/${missingPackerList}
+            missingPackerPrg=1
+        fi
+    done
+
+    # nice output with column. green=installed;red=notinstalled
+    echo -e "\n\n\nList of Programmes provided by \e[93mpacker\e[0m\n"
+    column -t -s "..." ${tmpDir}/${tmp2}
+    sleep 1     # just to slow things a bit down
 
     # if there are programmes missing (packer)
     if [[ ${missingPackerPrg} -eq 1 ]] ; then
@@ -219,10 +227,17 @@ function checkNeededProgrammes()
 
         if [[ ${instMissPrg} == "y" || ${instMissPrg} == "Y" ]] ; then
             # install all missing programmes one by one
-            while read -r line
+            index=0
+            while read line
             do
-                pacman -S $line
+                packerList[$index]="$line"
+                index=$(($index+1))
             done < ${tmpDir}/${missingPackerList}
+
+            for var in "${packerList[@]}"
+            do
+                packer --noedit --auronly -S ${var}
+            done
         elif [[ ${instMissPrg} == "n" || ${instMissPrg} == "N" || ${instMissPrg} == "" ]] ; then
             echo -e "\n\e[93m!!! There are missing programmes !!!"
             echo -e "Please install them by yourself and run this script again.\e[0m"
@@ -234,24 +249,67 @@ function checkNeededProgrammes()
         echo -e "\n\nEvery package maintainable by \e[93mpacker\e[0m \e[92mis installed\e[0m."
     fi
 
-    cleanup
+    unset instMissPrg
 }
 
 function cleanup()
 {
+    echo -e "Cleaning up ..."
     rm -rf ${tmpDir}/${tmp1}
     rm -rf ${tmpDir}/${tmp2}
+    rm -rf ${tmpDir}/${tmp3}
     rm -rf ${tmpDir}/${missingPacmanList}
     rm -rf ${tmpDir}/${missingPackerList}
+    echo -e "... done!"
+}
+
+function unsetVars()
+{
+    unset tmpDir
+    unset tmp1 tmp2 tmp3
     unset missingPacmanPrg
     unset missingPackerPrg
+    unset missingPacmanList
+    unset missingPackerList
+    unset packerList
+    unset repoPath
+    unset ulbin
+}
+
+function addToSudoers()
+{
+    sudo -v &> ${tmpDir}/${tmp3}
+    read -r line < ${tmpDir}/${tmp3}
+    if [[ ${line} == "" ]] ; then
+        echo -e "The user ${USER} is already member of 'sudoers'.\nNothing to do."
+    else
+        echo -e "It is mandatory to add the curring user running this script"
+        echo -e "to the sudoers file."
+        read -p "OK? [Y/n] : " addSudo
+
+        if [[ ${addSudo} == "y" || ${addSudo} == "Y" || ${addSudo} == "" ]] ; then
+            su -c "echo 'hringriin ALL=(ALL) ALL' >> /etc/sudoers" root
+        elif [[ ${addSudo} == "n" || ${addSudo} == "N" ]] ; then
+            echo "Aborting."
+            exit 3
+        else
+            echo "Unrecognized entry. Aborting."
+            exit 3
+        fi
+    fi
 }
 
 function main()
 {
+    cleanup
+    addToSudoers
+    checkNeededPacman
     installPacker
-    checkNeededProgrammes
+    checkNeededPacker
     linkConfigs
+    cleanup
+    unsetVars
+    exit 0
 }
 
 function installPacker()
@@ -259,7 +317,11 @@ function installPacker()
     if pacman -Qi packer &> /dev/null ; then
         echo -e "packer ... \e[92minstalled\e[0m" >> ${tmpDir}/${tmp1}
     else
-        mkdir ${tmpDir}/packer
+        echo -e "packer ... \e[91mnot minstalled\e[0m" >> ${tmpDir}/${tmp1}
+        if [[ ! -d ${tmpDir}/packer ]] ; then
+            mkdir ${tmpDir}/packer
+        fi
+        su -c "pacman -S wget" root
         wget -O ${tmpDir}/packer/packer.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/packer.tar.gz
         wget -O ${tmpDir}/packer/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
         cd ${tmpDir}/packer
@@ -271,47 +333,61 @@ function installPacker()
 function linkConfigs()
 {
     chmod go-rwx ~
-    sudo ln -s ${repoPath}/bash/bashrc /etc/bash.bashrc
-    sudo ln -s ${repoPath}/bash/myGitlabClone.bash ${ulbin}/myGitlabClone
-    sudo ln -s ${repoPath}/bash/youtubedl.bash ${ulbin}/youtubedl
-    ln -s ${repoPath}/git/gitconfig ~/.gitconfig
-    ln -s ${repoPath}/git/gitignore ~/.gitignore
+    sudo ln -fsv ${repoPath}/bash/bashrc /etc/bash.bashrc
+    sudo ln -fsv ${repoPath}/bash/myGitlabClone.bash ${ulbin}/myGitlabClone
+    sudo ln -fsv ${repoPath}/bash/youtubedl.bash ${ulbin}/youtubedl
+    ln -fsv ${repoPath}/git/gitconfig ~/.gitconfig
+    ln -fsv ${repoPath}/git/gitignore ~/.gitignore
 
     ${repoPath}/i3/createConfig.bash
 
-    mkdir --mode=700 ~/.mcabber_uni
-    ln -s ${repoPath}/mcabber/mcabberrc ~/.mcabber_uni/mcabberrc
+    if [[ ! -d ~/.mcabber_uni ]] ; then
+        mkdir --mode=700 ~/.mcabber_uni
+    fi
+    cp -ifv ${repoPath}/mcabber/mcabberrc ~/.mcabber_uni/mcabberrc
     echo -e "Please configure your jabber uni account config."
-    read -n 1 -s -p "Press any key to continue" 
+    read -n 1 -s -p "Press any key to continue"
     vim ~/.mcabber_uni/mcabberrc
 
-    mkdir --mode=700 ~/.mcabber_ccchb
-    ln -s ${repoPath}/mcabber/mcabberrc ~/.mcabber_ccchb/mcabberrc
+    if [[ ! -d ~/.mcabber_ccchb ]] ; then
+        mkdir --mode=700 ~/.mcabber_ccchb
+    fi
+    cp -ifv ${repoPath}/mcabber/mcabberrc ~/.mcabber_ccchb/mcabberrc
     echo -e "Please configure your jabber ccchb account config."
-    read -n 1 -s -p "Press any key to continue" 
+    read -n 1 -s -p "Press any key to continue"
     vim ~/.mcabber_ccchb/mcabberrc
 
-    mkdir --mode=700 ~/.local/rofi
-    ln -s ${repoPath}/rofi/config ~/.local/rofi/config
+    if [[ ! -d ~/.local/rofi ]] ; then
+        mkdir --mode=700 ~/.local/rofi
+    fi
+    ln -fsv ${repoPath}/rofi/config ~/.local/rofi/config
 
-    mkdir --mode=700 ~/.ssh
-    ln -s ${repoPath}/ssh/config
+    if [[ ! -d ~/.ssh ]] ; then
+        mkdir --mode=700 ~/.ssh
+    fi
+    cp -ifv ${repoPath}/ssh/config ~/.ssh/config
 
     echo -e "Please unencrypt the ssh config."
-    read -n 1 -s -p "Press any key to continue" 
+    read -n 1 -s -p "Press any key to continue"
     vim ~/.ssh/config
 
-    mkdir --mode=700 ~/.config/terminator
-    ln -s ${repoPath}/terminator/config ~/.config/terminator/config
+    if [[ ! -d ~/.config/terminator ]] ; then
+        mkdir --mode=700 ~/.config/terminator
+    fi
+    ln -fsv ${repoPath}/terminator/config ~/.config/terminator/config
 
-    ln -s ${repoPath}/TIMESCRIPT ~/TIMESCRIPT
+    ln -fsv ${repoPath}/TIMESCRIPT /home/${USER}/TIMESCRIPT
 
-    ln -s ${repoPath}/vim/vimrc /etc/vimrc
+    sudo ln -fsv ${repoPath}/vim/vimrc /etc/vimrc
 
-    ${repoPath}/weechat/link-files.bash
+    /home/${USER}/Repositories/github.com/hringriin/dotfiles/repo/weechat/link-files.bash
 
-    ln -s ${repoPath}/X/Xdefaults ~/.Xdefaults
-    ln -s ${repoPath}/X/xinitrc ~/.xinitrc
+    ln -fsv ${repoPath}/X/Xdefaults ~/.Xdefaults
+    ln -fsv ${repoPath}/X/xinitrc ~/.xinitrc
+
+    sleep 2
+
+    echo -e "This script exited normally."
 }
 
 # first action in this script. do not touch.
