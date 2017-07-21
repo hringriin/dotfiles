f08f12b6b6c983f9e2d31b4fe0c43de324b76144
Joschka Koester <jkoester@tzi.de>
2017-07-21 08:26:30 +0200

    [ systemd | isync ] created systemd services and timers for isync

--- mutt/create_config.bash
+++ mutt/create_config.bash
@@ -54,14 +54,51 @@ copyFiles()
     chmod -v -R og-rwx ${MUTTPATH} ~/.muttrc ~/.mbsyncrc
 }
 
+copyService()
+{
+    cp -fv ${MUTTREPOPATH}/../systemd/isync/* /etc/systemd/system/
+}
+
+startService()
+{
+    read -p "Enable service? [y/N]: " enableService
+    if [[ ${enableService} == "y" || ${enableServce} == "Y" ]] ; then
+        thisUser=`echo ${USER}`
+
+        echo "Enabling service ..."
+        #su -c "systemctl enable mbsync@${USER}.timer" root
+        sudo systemctl enable mbsync@${thisUser}.timer
+        echo "... service enabled!"
+
+        echo "Starting service ..."
+        #su -c "systemctl start mbsync@${USER}.timer" root
+        sudo systemctl start mbsync@${thisUser}.timer
+        echo "... service started!"
+
+        echo "Reloading systemctl daemon ..."
+        #su -c "systemctl daemon-reload" root
+        sudo systemctl daemon-reload
+        echo "... systemctl daemon reloaded!"
+    fi
+}
+
 main()
 {
+    if [[ ${UID} == 0 ]] ;
+        echo "You're root! Don't start this script as root!"
+        exit 4
+    fi
+
     checkIfExists owncloud
     checkIfExists mutt
     checkIfExists isync
     copyFiles
 
     ${MUTTREPOPATH}/../isync/create_config.bash
+
+    copyService
+    startService
 }
 
 main
+exit 0
