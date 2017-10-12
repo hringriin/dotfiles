3b115a96f0f835d410c175699649688ae300278c
Joschka Koester <jkoester@tzi.de>
2017-10-13 01:44:50 +0200

    [ chord book ] compile on server

---
+++ bash/chordbook-server-compile.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# server compile
+
+echo -e "\nSTARTING script: `date`"
+echo -e "Change directory ..."
+cd /home/hringriin/Repositories/github.com/hringriin/chordbook/repo/Unix/
+echo -e "Dir: `pwd`"
+
+echo -e "Pulling ..."
+git pull
+
+echo -e "Making ..."
+make -f /home/hringriin/Repositories/github.com/hringriin/chordbook/repo/Unix/Makefile server
+
+echo -e "\n\nDone!"
+echo -e "ENDING script: `date`\n"
