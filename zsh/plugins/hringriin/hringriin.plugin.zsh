bac26285299ab0ade37d75e0a58e7da55ece35c9
Joschka Koester <jkoester@tzi.de>
2017-10-12 19:42:58 +0200

    [ zsh new plugin ] hringriin
    my own plugin :)
    
    just importing aliases, functions and editor variables ;-)

---
+++ zsh/plugins/hringriin/hringriin.plugin.zsh
@@ -0,0 +1,8 @@
+# Aliases and Functions
+source /home/hringriin/Repositories/github.com/hringriin/dotfiles/repo/bash/bashrc.d/aliases
+source /home/hringriin/Repositories/github.com/hringriin/dotfiles/repo/bash/bashrc.d/functions
+
+# Editor
+#Arch specific for crontab
+export EDITOR=vim
+export VISUAL=vim
