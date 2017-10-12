0637cee4645564f1291d3ce26cc641a194011d62
Joschka Koester <jkoester@tzi.de>
2017-10-12 20:49:46 +0200

    [ bash | checkBatteryState ] outputs/echos now zsh conform

--- bash/checkBatteryState.zsh
+++ bash/checkBatteryState.zsh
@@ -19,12 +19,15 @@ fi
 
 if [ -f $AC ] ; then
     if [ `cat $AC` -eq 1 ] ; then
-        echo "$fg_bold[green]AC$reset_color"
+        #echo "$fg_bold[green]AC$reset_color"
+        echo "%B%F{green}AC%f%b"
     else
-        echo "$fg_bold[red]BAT$reset_color"
+        #echo "$fg_bold[red]BAT$reset_color"
+        echo "%B%F{red}BAT%f%b"
     fi
 else
-    echo "$fg_bold[yellow]N/A$reset_color"
+    #echo "$fg_bold[yellow]N/A$reset_color"
+    echo "%B%F{yellow}N/A%f%b"
 fi
 
 # Handling BAT 0 (Internal)
@@ -38,21 +41,21 @@ if [ -f $BAT0 ] ; then
     if [ $BAT0VAL -lt 100 ] ; then
         if [ $BAT0VAL -gt 50 ] ; then
             #echo -e "$fg_bold[green]$BAT0VAL %$reset_color"
-            echo -e "%B%F{green}$BAT0VAL%f%b"
+            echo -e "%B%F{green}$BAT0VAL%%%f%b"
         fi
     fi
 
     if [ $BAT0VAL -le 50 ] ; then
         if [ $BAT0VAL -gt 20 ] ; then
             #echo -e "$fg_bold[yellow]$BAT0VAL %$reset_color"
-            echo -e "%B%F{yellow}$BAT0VAL%f%b"
+            echo -e "%B%F{yellow}$BAT0VAL%%%f%b"
         fi
     fi
 
     if [ $BAT0VAL -le 20 ] ; then
         if [ $BAT0VAL -gt 0 ] ; then
             #echo -e "$fg_bold[red]$BAT0VAL %$reset_color"
-            echo -e "%B%F{red}$BAT0VAL%f%b"
+            echo -e "%B%F{red}$BAT0VAL%%%f%b"
         fi
     fi
 
@@ -75,21 +78,21 @@ if [ -f $BAT1 ] ; then
     if [ $BAT1VAL -lt 100 ] ; then
         if [ $BAT1VAL -gt 50 ] ; then
             #echo -e "$fg_bold[green]$BAT1VAL %$reset_color"
-            echo -e "%B%F{green}$BAT1VAL%f%b"
+            echo -e "%B%F{green}$BAT1VAL%%%f%b"
         fi
     fi
 
     if [ $BAT1VAL -le 50 ] ; then
         if [ $BAT1VAL -gt 20 ] ; then
             #echo -e "$fg_bold[yellow]$BAT1VAL %$reset_color"
-            echo -e "%B%F{yellow}$BAT1VAL%f%b"
+            echo -e "%B%F{yellow}$BAT1VAL%%%f%b"
         fi
     fi
 
     if [ $BAT1VAL -le 20 ] ; then
         if [ $BAT1VAL -gt 0 ] ; then
             #echo -e "$fg_bold[red]$BAT1VAL %$reset_color"
-            echo -e "%B%F{red}$BAT1VAL%f%b"
+            echo -e "%B%F{red}$BAT1VAL%%%f%b"
         fi
     fi
 
