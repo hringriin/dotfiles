424cbbb6273a66f2e4db4e2f30708471a67b3534
Joschka Köster <jkoester@tzi.de>
2018-03-21 15:05:56 +0100

    [ rofi create config script ]
    completely rewritten

--- rofi/create_config.bash
+++ rofi/create_config.bash
@@ -10,10 +10,20 @@ source INSTALL_ALL/config.bash
 
 main()
 {
-    if [[ ! ( -d ~/.local/rofi ) ]] ; then
-        mkdir --mode=700 -p ~/.local/rofi
+
+    if [[ ! ( -d ${HOME}/.config/rofi ) ]] ; then
+        mkdir --mode=700 -p ${HOME}/.config/rofi
     fi
-    ln -fsv ${repoPath}/rofi/config ~/.local/rofi/config
+    ln -fsv ${repoPath}/rofi/config ${HOME}/.config/rofi/config
+    ln -fsv ${repoPath}/rofi/gruvbox-dark-hringriin.rasi ${HOME}/.config/rofi/gruvbox-dark-hringriin.rasi
+
+    if [[ ! ( -d ${HOME}/.config/rofi-twitch ) ]] ; then
+        mkdir --mode=700 -p ${HOME}/.config/rofi-twitch
+        git clone git@github.com/hringriin/rofi-twitch ${HOME}/Repositories/github.com/hringriin/rofi-twitch/repo
+    fi
+
+    cp -v ${repoPath}/../../rofi-twitch/repo/settings ${HOME}/.config/rofi-twitch/settings
+    ln -s ${repoPath}/../../rofi-twitch/repo/hringriin-fav /usr/local/bin/hringriin-fav
 }
 
 main
