8ca22e66782f715c943ef24fb90b702b60acc6bd
Joschka Koester <jkoester@tzi.de>
2018-07-17 02:06:02 +0200

    [ i3 battery check ] revamping script for i3-battery check

--- bash/i3BattCheck.bash
+++ bash/i3BattCheck.bash
@@ -10,33 +10,40 @@ if [[ !(-e $tmpFile) ]] ; then
     exit 1
 fi
 
-echo $(env | grep DBUS)
-
 batt0=$(cat $tmpFile | grep 'Battery 0')
 batt1=$(cat $tmpFile | grep 'Battery 1')
 
-if [[ $(echo $batt0 | grep 'Discharging' | cut -f 5 -d " ") < 00:10:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "Battery 0 LOW!" "$batt0" --icon=battery-low
-elif [[ $(echo $batt0 | grep 'Discharging' | cut -f 5 -d " ") < 00:30:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "Battery 0 CRITICAL!" "batt0" --icon=battery-caution
-fi
+# some regex foo to get the precise battery level as integer
+function getBatLevel()
+{
+    echo ${1} | cut -d ',' -f 2 | sed -e 's/  *//g' | tr -d '%'
+}
 
-if [[ $(echo $batt1 | grep 'Discharging' | cut -f 5 -d " ") < 00:10:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "Battery 1 LOW!" "$batt1" --icon=battery-low
-elif [[ $(echo $batt1 | grep 'Discharging' | cut -f 5 -d " ") < 00:30:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "Battery 1 CRITICAL!" "$batt1" --icon=battery-caution
-fi
+function checkBatt()
+{
+    # if the battery is not available (plugged in / attached), nothing will happen
+    if [[ $1 != "" ]] ; then
+        # Check, if the battery is discharching.
+        if [[ $(echo $1 | cut -d ',' -f 1) == *"Discharging" ]] ; then
+            # Check, if the battery level is below 10 percent.
+            # if true, issue notify-send and i3-nagbar to make the user aware
+            # of the critical battery level
+            if [[ $(getBatLevel "$1") < 10 ]] ;then
+                DISPLAY=:0.0 notify-send --icon=battery-caution "Battery 0 is CRITICAL!" "$1"
+                i3-msg fullscreen disable
+                i3-nagbar -t error -m "Battery 0 is CRITICAL! $1" -f "pango:DejaVu Sans Mono 16"
+            # Check, if the battery level is below 25 percent.
+            # if true, issue notify-send to make the user aware of the low
+            # battery level
+            elif [[ $(getBatLevel "$1") < 25 ]] ;then
+                DISPLAY=:0.0 notify-send --icon=battery-low "Battery 0 is low!" "$1"
+                i3-nagbar -t warning -m "Battery 0 is low! $1" -f "pango:DejaVu Sans Mono 10"
+            fi
+        fi
+    fi
+}
 
+checkBatt "${batt0}"
+checkBatt "${batt1}"
 rm -rf $tmpFile
-
-#if [[ `echo $BatInfo | grep 'Battery 0' && `echo $BatInfo | cut -f 5 -d " "` < 00:15:00 ]] ; then
-#    DISPLAY=:0.0 /usr/bin/notify-send "Battery 0 LOW!" "$BatInfo"
-#else
-#    DISPLAY=:0.0 /usr/bin/notify-send "works!" "$BatInfo"
-#fi
-#
-#if [[ `echo $BatInfo | grep 'Battery 1' && `echo $BatInfo | cut -f 5 -d " "` < 00:15:00 ]] ; then
-#    DISPLAY=:0.0 /usr/bin/notify-send "Battery 1 LOW!" "$BatInfo"
-#else
-#    DISPLAY=:0.0 /usr/bin/notify-send "works!" "$BatInfo"
-#fi
+exit 0
