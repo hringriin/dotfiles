7b88d8483e66f0dbe210cab686a990d0f72ea98d
jkoester <jkoester@tzi.de>
2018-07-27 02:04:03 +0200

    [ zsh | plugin: hringriin ] vim foldings

--- zsh/plugins/hringriin/hringriin.plugin.zsh
+++ zsh/plugins/hringriin/hringriin.plugin.zsh
@@ -1,25 +1,30 @@
 # vim: ft=sh
-# Generic paths seem to work fine, even on MacOS
+
+# Generic paths seem to work fine, even on MacOS {{{
 source ${HOME}/Repositories/github.com/hringriin/dotfiles/repo/bash/bashrc.d/aliases
 source ${HOME}/Repositories/github.com/hringriin/dotfiles/repo/bash/bashrc.d/functions
 tmuxi="${HOME}/Repositories/github.com/hringriin/dotfiles/repo/tmux/tmuxinator"
+# Generic paths seem to work fine, even on MacOS }}}
 
-# Editor
+# Editor {{{
 export EDITOR=vim
 export VISUAL=vim
+# Editor }}}
 
-# Homebrew token
+# Homebrew token {{{
 if [[ -e ${HOME}/.github-homebrew-token ]] ; then
     source ${HOME}/.github-homebrew-token
 fi
+# Homebrew token }}}
 
-# tmuxinator
+# tmuxinator {{{
 if [[ -d /home/hringriin/.gem/ruby/2.5.0/bin ]] ; then
     # adding ruby bin to path variable
     export PATH=/home/hringriin/.gem/ruby/2.5.0/bin:${PATH}
 fi
+# tmuxinator }}}
 
-# ccache
+# ccache {{{
 if [[ -d /usr/lib/ccache/bin ]] ; then
     export PATH=/usr/lib/ccache/bin/:${PATH}
 fi
@@ -28,15 +33,19 @@ if [[ -e ${tmuxi}/completion/tmuxinator.zsh ]] ; then
     # sourcing tmuxinator completion
     source ${tmuxi}/completion/tmuxinator.zsh
 fi
+# ccache }}}
 
-# alises vor zsh autocorrection. sometimes it this is seriously p*** me off.
+# prevent tmux autocorrection for certain commands {{{
 alias 'cd ...'='nocorrect cd ../..'
 alias 'tmux'='nocorrect tmux'
+# prevent tmux autocorrection for certain commands }}}
 
-# MACOS shit ...
+# MACOS shit ... {{{
 if [[ $(uname -a) == *"arwin"* ]] ; then
     alias 'vim'='/usr/local/Cellar/vim/8.0.1553_2/bin/vim'
 fi
+# MACOS shit ... }}}
+
 # Fuzzy Finder (fzf) {{{
 fzf_history()
 {
