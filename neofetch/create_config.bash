13457efeb3bdd305c9cf5c17ccabd180107f4b2b
Joschka Koester <jkoester@tzi.de>
2018-08-27 17:46:51 +0200

    [ neofetch ] adding config

---
+++ neofetch/create_config.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+# create_config for -- <<PROGNAME>>
+
+prgname="neofetch"
+
+echo -e "\e[1;36mInstalling ... ${prgname} ... configuration files ...\e[0m"
+sleep 1
+
+source INSTALL_ALL/config.bash
+source INSTALL_ALL/packer.bash
+source INSTALL_ALL/pacman.bash
+
+main()
+{
+    ln -sfv ${repoPath}/neofetch/config.conf ${HOME}/.config/neofetch/config.conf
+}
+
+main
+exit 0
