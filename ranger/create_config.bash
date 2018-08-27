138b816336745c5cf4d705e192aadd21af5cf82a
Joschka Koester <jkoester@tzi.de>
2018-08-27 17:52:58 +0200

    [ ranger ] config added

---
+++ ranger/create_config.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+# create_config for -- <<PROGNAME>>
+
+prgname="ranger"
+
+echo -e "\e[1;36mInstalling ... ${prgname} ... configuration files ...\e[0m"
+sleep 1
+
+source INSTALL_ALL/config.bash
+source INSTALL_ALL/packer.bash
+source INSTALL_ALL/pacman.bash
+
+main()
+{
+    if [[ ! -d ${HOME}/.config/ranger ]] ; then
+        mkdir -p ${HOME}/.config/ranger
+    fi
+
+    ln -sfv ${repoPath}/ranger/bookmarks ${HOME}/.config/ranger/
+    ln -sfv ${repoPath}/ranger/commands.py ${HOME}/.config/ranger/
+    ln -sfv ${repoPath}/ranger/commands_full.py ${HOME}/.config/ranger/
+    ln -sfv ${repoPath}/ranger/history ${HOME}/.config/ranger/
+    ln -sfv ${repoPath}/ranger/rc.conf ${HOME}/.config/ranger/
+    ln -sfv ${repoPath}/ranger/rifle.conf ${HOME}/.config/ranger/
+    ln -sfv ${repoPath}/ranger/scope.sh ${HOME}/.config/ranger/
+    ln -sfv ${repoPath}/ranger/tagged ${HOME}/.config/ranger/
+}
+
+main
+exit 0
