9a19b0805b5fa128598f0df70bb979be386dcb11
Joschka Köster <jkoester@tzi.de>
2018-10-12 22:52:52 +0200

    [ zsh | install ] setting echoed strings to something useful
    ... instead of echoing "FIRST", "SECOD", ...

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -12,7 +12,7 @@ ln -s ~/Repositories/github.com/hringriin/dotfiles/repo/zsh/plugins/hringriin/ .
 cd ~/.oh-my-zsh/themes/
 ln -s ~/Repositories/github.com/hringriin/dotfiles/repo/zsh/themes/hringriin.zsh-theme ./
 
-echo "FIRST"
+echo "setting theme"
 cat ${zsh} | sed -e 's/ZSH_THEME=".*"/ZSH_THEME="hringriin"/g' > ${tmpf}
 cp ${tmpf} ${zsh}
 
@@ -20,19 +20,20 @@ echo "setting hyphen insensitive"
 cat ${zsh} | sed -e 's/# HYPHEN_INSENSITIVE/HYPHEN_INSENSITIVE/g' > ${tmpf}
 cp ${tmpf} ${zsh}
 
-echo "THIRD"
+echo "enable autocorrection"
 cat ${zsh} | sed -e 's/# ENABLE_CORRECTION/ENABLE_CORRECTION/g' > ${tmpf}
 cp ${tmpf} ${zsh}
 
-echo "FOURTH"
+echo "enable completing dots"
 cat ${zsh} | sed -e 's/# COMPLETION_WAITING_DOTS/COMPLETION_WAITING_DOTS/g' > ${tmpf}
 cp ${tmpf} ${zsh}
 
-echo "FIFTH"
+echo "setting history timestamp format"
 cat ${zsh} | sed -e 's/# HIST_STAMPS.*/HIST_STAMPS="yyyy-mm-dd"/g' > ${tmpf}
 cp ${tmpf} ${zsh}
 
-echo "SIXTH"
+echo "setting plugin list"
+echo "You need to edit line 62 of .zshrc manually!"
 cat ${zsh} | sed -e 's/plugins=(.*)/plugins=(colored-man-pages hringriin mosh screen zsh-syntax-highlighting)/g' > ${tmpf}
 cp ${tmpf} ${zsh}
 
