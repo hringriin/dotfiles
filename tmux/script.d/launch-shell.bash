d63b0ad1eb44f98c320839169e9a58bd9e563b1e
Joschka Koester <jkoester@tzi.de>
2018-10-16 15:21:39 +0200

    [ tmuxinator | launch shell ] improved startup
    formerly, the script checked whether tmux instances are running and if
    one of them is called "main".
    
    from now on, the script will check, if several terminals are installed.
    the more exotic ones / the preferred ones first. after that, the shell
    is started. if there is no tmux session called "main", the terminal
    emulator is started with an option to call a script itself.
    
    the `launch-shell.bash` script is now also able to call a filemanager
    (fm) called *ranger*.

--- tmux/script.d/launch-shell.bash
+++ tmux/script.d/launch-shell.bash
@@ -1,8 +1,56 @@
 #!/bin/bash
 # launch-shell
 
-if [[ $(tmux ls | grep main | grep attached) == "" ]] ; then
-    termite -e ~/Repositories/github.com/hringriin/dotfiles/repo/tmux/script.d/launch-tmux.bash
-else
-    termite
-fi
+myShell=
+
+# select an available (installed) terminal emulator
+function selShell ()
+{
+    if [[ $(command -v termite) > /dev/null ]] ; then
+        myShell=termite
+    elif [[ $(command -v terminator) > /dev/null ]] ; then
+        myShell=terminator
+    elif [[ $(command -v tilix) > /dev/null ]] ; then
+        myShell=tilix
+    elif [[ $(command -v xterm) > /dev/null ]] ; then
+        myShell=xterm
+    else
+        exit 1
+    fi
+}
+
+# start either just the emulator or the emulator calling the tmuxinator-script
+function startShell ()
+{
+    if [[ $(tmux ls | grep main | grep attached) == "" ]] ; then
+        ${myShell} -e ~/Repositories/github.com/hringriin/dotfiles/repo/tmux/script.d/launch-tmux.bash
+    else
+        ${myShell}
+    fi
+    #${myShell}
+}
+
+# start the filemanager
+function startFm ()
+{
+    ${myShell} -e ranger
+}
+
+
+# if the first parameter was "term", start a terminal, if it was "fb" start ranger (filemanager)
+function main ()
+{
+    if [[ $1 == "term" ]] ; then
+        selShell
+        startShell
+    elif [[ $1 == "fm" ]] ; then
+        selShell
+        startFm
+    else
+
+        exit 2
+    fi
+}
+
+main $1
+exit 0
