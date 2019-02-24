dba69f2b5c646f7afbee1c3b0389b852cc6cb025
Joschka Koester <jkoester@tzi.de>
2019-02-24 03:20:27 +0100

    [ Qutebrowser | script video spool ] adding flags to script
    the flag `-s` allows to add the video to the `tsp-youtube` task-spooler
    instead of feeding it directly to mpv

--- qutebrowser/userscripts/video-spool.bash
+++ qutebrowser/userscripts/video-spool.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
 # video-spool - add a video (e.g. Youtube) to the youtube-task-spooler
 
-TS_SOCKET=/tmp/tsp-youtube tsp mpv ${QUTE_URL}
+while getopts ":s" opt ; do
+    case $opt in
+        s)
+            # let task-spooler handle it
+            TS_SOCKET=/tmp/tsp-youtube tsp mpv ${QUTE_URL}
+            exit 0
+            ;;
+    esac
+done
+
+mpv ${QUTE_URL}
+exit 0
