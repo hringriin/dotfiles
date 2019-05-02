d1e54178d8a8fc5faef17adb3364b111933fc6e4
Joschka Koester <hringriin@gmail.com>
2019-05-03 00:25:02 +0200

    [ st | create config ] removing diff files after patching

--- st/create_config.bash
+++ st/create_config.bash
@@ -33,6 +33,7 @@ main()
     wget https://st.suckless.org/patches/copyurl/st-copyurl-20190202-3be4cf1.diff
     patch -Np1 -i st-alpha-0.8.2.diff
     patch -Np1 -i st-copyurl-20190202-3be4cf1.diff
+    rm *.diff*
     ln -fsv ${repoPath}/st/config.h ${stRepo}/config.h
     ln -fsv ${repoPath}/st/st.info ${stRepo}/st.info
 
