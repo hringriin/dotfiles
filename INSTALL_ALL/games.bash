a8568dfea5a3dca6a3ee5f429de4d1c9ce48dbf0
Joschka Koester <hringriin@gmail.com>
2019-05-19 17:15:07 +0200

    [ packages ] adding packages to list
    also adding games as separate list

---
+++ INSTALL_ALL/games.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+# games.bash
+
+# list of games
+pacmanGames=(
+    '0ad'
+    '0ad-data'
+    'curseofwar'
+    'nethack'
+    'simutrans'
+    'warzone2100'
+    'wesnoth'
+)
+
+yayGames=(
+    'ballerburg'
+    'dangerdeep'
+    'dangerdeep-data'
+    'nudoku-git'
+)
