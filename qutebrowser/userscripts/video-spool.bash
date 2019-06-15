3e14467b3ed096fe704c64609ff29f17538bdc20
Joschka Koester <hringriin@gmail.com>
2019-06-15 20:37:30 +0200

    [ qutebrowser | video ] correcting keybindings and video-spool script
    the video spool script will be split in two parts, one for just playing
    with mpv and one for adding the video to the task-spooler.
    
    the keybindings were changed respectively, because it seems not to be
    possible to have a script called with arguments or flags such as
    `./video-spoo.bash -s`

--- qutebrowser/userscripts/video-spool.bash
+++ qutebrowser/userscripts/video-spool.bash
@@ -1,15 +1,5 @@
 #!/bin/bash
 # video-spool - add a video (e.g. Youtube) to the youtube-task-spooler
 
-while getopts ":s" opt ; do
-    case $opt in
-        s)
-            # let task-spooler handle it
-            TS_SOCKET=/tmp/tsp-youtube tsp mpv ${QUTE_URL}
-            exit 0
-            ;;
-    esac
-done
-
-mpv ${QUTE_URL}
+TS_SOCKET=/tmp/tsp-youtube tsp mpv ${QUTE_URL}
 exit 0
---
+++ qutebrowser/userscripts/video.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+# video - view a video with mpv (implicit youtube-dl)
+
+mpv ${QUTE_URL}
+exit 0
