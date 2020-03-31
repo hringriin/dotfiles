afcab03c255b8e9e6607e8dc339d0c9081fb1220
Joschka Köster <koester.joschka@gmail.com>
2020-03-31 17:47:50 +0200

    [ install-all.bash ] correcting read statement
    -r is wrong, -p is the correct flag

--- install-all.bash
+++ install-all.bash
@@ -29,7 +29,7 @@ function ownCloudInstall()
     echo -e "\e[1;31mYou need to install 'ownCloud' manually first!\e[0m"
     echo -e "\e[1;31mDo it now! Use a seperate terminal for this operation!\e[0m"
     echo -e "\e[1;31mYou won't be able to install mutt or isync/mbsync without ownCloud!\e[0m\n\n"
-    read -r "Press any key to continue."
+    read -p "Press any key to continue."
 }
 
 # information regarding arch linux
