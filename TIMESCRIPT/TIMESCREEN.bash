101bfc121efa88bcd4315ac4221130c3e28440e4
Joschka Köster <koester.joschka@gmail.com>
2020-04-06 16:31:27 +0200

    [ screen-message | timescreen ] changing fg and bg colour

--- TIMESCRIPT/TIMESCREEN.bash
+++ TIMESCRIPT/TIMESCREEN.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-( while sleep 1 ; do /home/hringriin/TIMESCRIPT/TIMESCRIPT.bash ; echo -e '\f' ; done ) | sm --foreground=#00FF00 --background=#000000 -
+( while sleep 1 ; do /home/hringriin/TIMESCRIPT/TIMESCRIPT.bash ; echo -e '\f' ; done ) | sm --foreground=#ebdbb2 --background=#282828 -
