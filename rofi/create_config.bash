06d2a754ef49c4e825c2e3425d5d2fc53ecc86da
Joschka Köster <koester.joschka@gmail.com>
2020-10-08 04:49:13 +0200

    [ rofi ] new config format and filename

--- rofi/create_config.bash
+++ rofi/create_config.bash
@@ -14,17 +14,11 @@ main()
     if [[ ! ( -d ${HOME}/.config/rofi ) ]] ; then
         mkdir --mode=700 -p ${HOME}/.config/rofi
     fi
-    ln -fsv ${repoPath}/rofi/config ${HOME}/.config/rofi/config
+    ln -fsv ${repoPath}/rofi/config.rasi ${HOME}/.config/rofi/config.rasi
     ln -fsv ${repoPath}/rofi/gruvbox_hringriin.rasi ${HOME}/.config/rofi/gruvbox_hringriin.rasi
     ln -fsv ${repoPath}/rofi/gruvbox-dark-hringriin.rasi ${HOME}/.config/rofi/gruvbox-dark-hringriin.rasi
 
-    if [[ ! ( -d ${HOME}/.config/rofi-twitch ) ]] ; then
-        mkdir --mode=700 -p ${HOME}/.config/rofi-twitch
-        git clone git@github.com:hringriin/rofi-twitch ${HOME}/Repositories/github.com/hringriin/rofi-twitch/repo
-    fi
-
     cp -v ${repoPath}/../../rofi-twitch/repo/settings ${HOME}/.config/rofi-twitch/settings
-    sudo ln -s ${repoPath}/../../rofi-twitch/repo/hringriin-fav /usr/local/bin/hringriin-fav
 }
 
 main
