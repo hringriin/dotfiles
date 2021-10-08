ae3186f0be235cb1a96c7243ec1993be4f5407d0
Joschka Köster <koester.joschka@gmail.com>
2021-10-08 12:16:26 +0200

    [ rofi ] configuration will be updated

--- rofi/create_config.bash
+++ rofi/create_config.bash
@@ -15,8 +15,7 @@ main()
         mkdir --mode=700 -p ${HOME}/.config/rofi
     fi
     ln -fsv ${repoPath}/rofi/config.rasi ${HOME}/.config/rofi/config.rasi
-    ln -fsv ${repoPath}/rofi/gruvbox_hringriin.rasi ${HOME}/.config/rofi/gruvbox_hringriin.rasi
-    ln -fsv ${repoPath}/rofi/gruvbox-dark-hringriin.rasi ${HOME}/.config/rofi/gruvbox-dark-hringriin.rasi
+    ln -fsv ${repoPath}/rofi/hringriin.rasi ${HOME}/.config/rofi/hringriin.rasi
 
     cp -v ${repoPath}/../../rofi-twitch/repo/settings ${HOME}/.config/rofi-twitch/settings
 }
